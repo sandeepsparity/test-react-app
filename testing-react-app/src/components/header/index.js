5fa9cbfe745893493510d122b1b5f1ae02095284
sandeep reddy <sandeep.contractor@squarepanda.com>
2020-07-23 15:31:42 -0700

    Initial Commit

---
+++ testing-react-app/src/components/header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.scss';
+
+const Header = ({ title }) => {
+    return (
+    <header>
+        <div className='wrap'>
+            <div className='logo'></div>
+            </div>
+    </header>
+    )
+}
+
+export default Header;
